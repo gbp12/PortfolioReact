ae48d8364858b1addddd6fb797a5d0c751624907
gbp12 <gonzabonilla4896@outlook.es>
2022-02-16 17:02:43 +0000

    images try 1

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -9,7 +9,7 @@ export const Skills = () => {
             <Img src="/images/html.png" alt="foto de html" />
             <Img src="/images/css (1).png" />
             <Img src="/images/js.png" />
-            <Rotation src="/images/react.png" />
+            <Rotation src="/public/images/react.png" />
             <Img  src="/images/python.png" />
             <Img  src="/images/git logo.png" />
             
