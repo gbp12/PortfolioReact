dd1478108c41253e3fb011535e489fe826312992
gbp12 <gonzabonilla4896@outlook.es>
2022-02-16 19:18:43 +0000

    imagenes arregladas

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -2,10 +2,11 @@ import {Div, Img, Rotation} from "./Styles"
 
 
 
-
+//logos de las skills
 export const Skills = () => {
     return (
         <Div>
+            
             <Img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/1200px-HTML5_logo_and_wordmark.svg.png" alt="foto de html" />
             <Img src="https://lineadecodigo.com/wp-content/uploads/2014/04/css.png" />
             <Img src="https://serobo.com/assets/img/javascript2.png" />
